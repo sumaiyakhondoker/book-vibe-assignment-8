f465d4e0d8321aa55b099f80335e8ae835b8f493
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2024-11-09 15:53:03 +0600

    contact us route added successfully

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,15 @@
-
+import { GoDotFill } from "react-icons/go";
 
 const AboutUs = () => {
     return (
-        <div>
-            <h2>this is about us route</h2>
+        <div className="mt-5 px-3 lg:px-0">
+            {/* upper part */}
+            <div className="pb-4 flex flex-col items-center">
+        <h3 className="text-3xl font-bold">About Us</h3>
+        <p className="max-w-4xl mx-auto text-center mt-2">“Welcome to <span className="font-bold">Book Vibe</span> , where every story finds its reader! Our passion is to bring books and readers together, offering a curated collection of timeless classics, contemporary favorites, and hidden gems.</p>
+      </div>
+      {/* lower part */}
+      
         </div>
     );
 };
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,11 +1,108 @@
-
+import toast from "react-hot-toast";
 
 const ContactUs = () => {
-    return (
-        <div>
-            <h2>this is contact us page</h2>
+    const handleSubmit = () =>{
+        toast.success('Submitted Successfully!')
+    }
+  return (
+    <div className="mt-5">
+      {/* upper part */}
+      <div className="pb-4 flex flex-col items-center">
+        <h3 className="text-3xl font-bold">Contact Us</h3>
+        <p className="max-w-3xl mx-auto text-center mt-2">“We’d love to hear from you! Whether you have a question, feedback, or need assistance with your order, feel free to reach out to us using the details below.”</p>
+      </div>
+
+      {/* lower part */}
+      <section className="py-6 lg:py-16    dark:bg-gray-100 dark:text-gray-900  rounded-xl shadow-xl bg-slate-100">
+        <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+          <div className="py-6 md:py-0 md:px-6">
+            <h1 className="text-4xl font-bold">Get in touch</h1>
+            <p className="pt-2 pb-4">
+              Fill in the form to start a conversation
+            </p>
+            <div className="space-y-4">
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6 "
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>
+                  583 Novel Avenue,  United States
+                </span>
+              </p>
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6"
+                >
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                </svg>
+                <span>(555) 123-4567</span>
+              </p>
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6"
+                >
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                </svg>
+                <span>support@bookvibe.com</span>
+              </p>
+            </div>
+          </div>
+          <form
+            noValidate=""
+            className="flex flex-col py-6 space-y-6 md:py-0 md:px-6"
+          >
+            <label className="block">
+              <span className="mb-1">Full name</span>
+              <input
+                type="text"
+                required
+                placeholder="Leroy Jenkins"
+                className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100 p-2"
+              />
+            </label>
+            <label className="block">
+              <span className="mb-1">Email address</span>
+              <input
+                type="email"
+                required
+                placeholder="leroy@jenkins.com"
+                className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100 p-2"
+              />
+            </label>
+            <label className="block">
+              <span className="mb-1">Message</span>
+              <textarea
+                rows="3"
+                className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100"
+              ></textarea>
+            </label>
+            <button onClick={handleSubmit}
+              type="button"
+              className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-[#59C6D2] text-white dark:bg-violet-600 dark:text-gray-50 focus:dark:ring-violet-600 hover:dark:ring-violet-600"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default ContactUs;
